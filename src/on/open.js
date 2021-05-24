c8ba99900b45e0b215ea9e584aab45afac9335c9
shaaaaaQ <64454953+shaaaaaQ@users.noreply.github.com>
2021-05-25 00:46:00 +0900

    console.log

--- src/on/open.js
+++ src/on/open.js
@@ -2,10 +2,20 @@ module.exports = async function () {
     this.connect('homeTimeline');
     this.connect('main');
     const i = await this.post('i');
+    const followedChannel = await this.post('channels/followed', { limit: 100 });
+    console.log('--------------------------------------------------');
     if (i) {
-        console.log(`--------------------------------------------------
-userId   : ${i.id}
-username : ${i.username}
---------------------------------------------------`);
+        console.log('<アカウント情報>');
+        console.log(` userId   : ${i.id}`);
+        console.log(` username : ${i.username}`);
+        console.log(` follower : ${i.followersCount}`);
+        console.log(` following: ${i.followingCount}`);
     }
+    if (followedChannel.length) {
+        console.log('<フォロー中のチャンネル>');
+        followedChannel.forEach(ch => {
+            console.log(` ${ch.name}`);
+        });
+    }
+    console.log('--------------------------------------------------');
 };
